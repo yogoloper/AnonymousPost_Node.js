3b05f0388a4751ddfa576984bae118a2bd45f898
yogoloper <jguddnjs@gmail.com>
2022-06-29 12:33:22 +0900

    refac: typeorm config에 ConfigModule 적용

--- src/common/configs/typeorm.config.ts
+++ src/common/configs/typeorm.config.ts
@@ -1,6 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
+ConfigModule.forRoot({ isGlobal: true });
+
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DATABASE_HOST,
@@ -8,6 +10,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  timezone: process.env.DATABASE_TIMEZONE,
   entities: [__dirname + '/../../**/*.entity{.js,.ts}'],
   synchronize: true,
   logging: true,
