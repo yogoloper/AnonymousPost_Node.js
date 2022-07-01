878c1f9e384199fef16df19edd8ff0bf7b4ee784
yogoloper <jguddnjs@gmail.com>
2022-07-01 23:19:15 +0900

    refac: .env 없이도 기본 DB로 연결 되도록 config 값 추가

--- src/common/configs/typeorm.config.ts
+++ src/common/configs/typeorm.config.ts
@@ -5,13 +5,13 @@ ConfigModule.forRoot();
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  timezone: process.env.DATABASE_TIMEZONE,
+  host: process.env.DATABASE_HOST || 'localhost',
+  port: parseInt(process.env.DATABASE_PORT) || 3306,
+  username: process.env.DATABASE_USERNAME || 'root',
+  password: process.env.DATABASE_PASSWORD || '1234',
+  database: process.env.DATABASE_NAME || 'anonymous_post',
+  timezone: process.env.DATABASE_TIMEZONE || 'Asia/Seoul',
   entities: [__dirname + '/../../**/*.entity{.js,.ts}'],
   synchronize: true,
-  logging: true,
+  logging: false,
 };
